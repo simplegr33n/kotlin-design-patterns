25ab8715057414d3fa6a8a8d61f8d42027aecf34
Geoff Golda <gcgolda@gmail.com>
2019-02-14 01:29:07 -0700

    [01] override toString() of Singleton Object

--- src/_01_singleton/_01_basic_singleton.kt
+++ src/_01_singleton/_01_basic_singleton.kt
@@ -1,9 +1,11 @@
 package _01_singleton
 
 var singletonInstance: SingletonExample? = null
-var testValue: Int = 0
+var timesInitialized: Int = 0
 
 fun main() {
+    println(SingletonExample)
+
     println("First Try")
     SingletonExample.tryInitSingleton()
 
@@ -12,22 +14,35 @@ fun main() {
 
     println("Third Try")
     SingletonExample.tryInitSingleton()
+
+    println(singletonInstance)
 }
 
 object SingletonExample {
 
-    private var value: Int? = null
+    private var singletonValue: Int? = null
 
     fun tryInitSingleton() {
         if (singletonInstance == null) {
             singletonInstance = SingletonExample
-            testValue++
-            value = testValue
+            timesInitialized++
         }
 
+        singletonValue = timesInitialized
         val hashCode = singletonInstance.hashCode()
 
-        println("Singleton Value: $value, HashCode: $hashCode")
+        println("Singleton Value: $singletonValue, HashCode: $hashCode")
+    }
+
+    override fun toString(): String {
+        when (singletonValue) {
+            0 -> return "Impossible! SingletonExample object #$singletonValue, oh no..."
+            1 -> return "SingletonExample object #$singletonValue, great!"
+            // below is a range trick using Int.MAX_VALUE basically equating to if (singletonValue >= 2)
+            in 2..Int.MAX_VALUE -> return "Impossible! SingletonExample object #$singletonValue, oh no..."
+            // else is null (uninitialized state)
+            else -> return "$singletonValue object"
+        }
     }
 
 }
